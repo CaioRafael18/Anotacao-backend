83b3733d33127df0c7af3a2d9f6590283c820b18
Caio Rafael <caiorafaelbelarmino1@gmail.com>
2024-01-17 16:05:13 -0300

    ajuste na rota

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express")
-const routes = require("./routes")
+const routes = require("./src/routes")
 const cors = require("cors")
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
-const dbConfig = require('../config/dbConfig')
+const dbConfig = require('./config/dbConfig')
 dbConfig()
 
 app.use(cors())
 app.use(express.json());
 app.use(routes)
+
+app.use("/teste", (request, response) => {
+    response.send("Teste")
+})
 
 app.listen(3333, function(){
     console.log("Servidor online!")
